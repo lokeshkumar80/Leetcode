396e9978eb5865ad35f7d8be199b261b9c8a5dee
Lokesh Kumar <lokeshkumar797877@gmail.com>
2025-02-28 10:18:53 +0530

    DSA Problem

---
+++ 3151_problem.cpp
@@ -0,0 +1,53 @@
+// class Solution {
+//     public:
+//         bool evencheckspecial(vector<int>& nums){
+//             int n = nums.size();
+//             int i=0;
+//             while(i<n){
+//                 if(i%2 == 0){
+//                     if(nums[i]%2 != 0){
+//                         return false;
+//                     }
+//                 }
+//                 else{
+//                     if(nums[i]%2 == 0){
+//                         return false;
+//                     }
+//                 }
+//                 i++;
+//             }
+//             return true;
+//         }
+    
+//         bool oddcheckspecial(vector<int>& nums){
+//             int n = nums.size();
+//             int i=0;
+//             while(i<n){
+//                 if(i%2 == 0){
+//                     if(nums[i]%2 == 0){
+//                         return false;
+//                     }
+//                 }
+//                 else{
+//                     if(nums[i]%2 != 0){
+//                         return false;
+//                     }
+//                 }
+//                 i++;
+//             }
+//             return true;
+//         }
+    
+//         bool isArraySpecial(vector<int>& nums) {
+//             int n = nums.size();
+//             int i=0;
+//             if(nums[0] % 2 == 0){
+//                 bool ans1 = evencheckspecial(nums);
+//                 return ans1;
+//             }
+//             else{
+//                 bool ans2 = oddcheckspecial(nums);
+//                 return ans2;
+//             }
+//         }
+//     };
