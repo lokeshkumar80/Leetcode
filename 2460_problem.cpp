aaeaccf4c1653f8c04a05d0e5e6ada7ceea67382
Lokesh Kumar <lokeshkumar797877@gmail.com>
2025-03-01 20:58:12 +0530

    DSA Problem

---
+++ 2460_problem.cpp
@@ -0,0 +1,28 @@
+// class Solution {
+//     public:
+//         vector<int> applyOperations(vector<int>& nums) {
+//             int n=nums.size();
+//             vector<int>ans;
+//             int i=0;
+//             int temp;
+//             while(i<n){
+//                 if(i+1<n && nums[i]!=0 && nums[i]==nums[i+1]){
+//                     temp = 2*nums[i];
+//                     nums[i] = temp;
+//                     ans.push_back(temp);
+//                     nums[i+1] = 0;
+//                 }
+//                 else if(nums[i]!=0){
+//                     ans.push_back(nums[i]);
+//                 }
+//                 i++;
+//             }
+            
+//             int m = ans.size();
+//             while(m<n){
+//                 ans.push_back(0);
+//                 m++;
+//             }
+//             return ans;
+//         }
+//     };
